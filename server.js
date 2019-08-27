3d21ee6ede2a0a3f6fcdc7ac2bab6826dd22acb4
candicecz <czech.candice@gmail.com>
2019-08-27 09:52:59 -0400

    [REMOVE] app redirect

--- server.js
+++ server.js
@@ -4,15 +4,6 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.all(/.*/, (req, res, next) => {
-  const host = req.header("host");
-  if (host.match(/^www\..*/i)) {
-    next();
-  } else {
-    res.redirect(301, "http://www." + host + req.url);
-  }
-});
-
 app.use(express.static(`${__dirname}/dist`));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
