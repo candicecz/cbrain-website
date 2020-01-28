56c4971e6469a56e7ab156f847794274d2cc6b52
candicecz <czech.candice@gmail.com>
2020-01-28 13:55:53 -0500

    [UPDATE] Projects

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,61 +1,39 @@
+/* eslint-disable  */
 import "./projects.scss";
 import "../partials/partials";
-import projectData from "../projects-data.json";
 
+const toggleDrawer = e => {
+  // Close previously opened drawers;
+  const id = e.currentTarget.id;
+  Array.from(document.querySelectorAll("[data-selected]")).map(el => {
+    !el.id.includes(id)
+      ? (el.dataset.selected = false)
+      : (el.dataset.selected = !JSON.parse(el.dataset.selected));
+  });
 
-const loadContent = () => {
-  Array.from(document.querySelectorAll(".project")).map((_) => {
-    const content_el = _.querySelector(".project-content");
-    const description_el = _.querySelector(".project-content-text");
-    const description_height = description_el.getBoundingClientRect().height;
-    content_el.style.height = `${0}px`;
-    description_el.style.marginTop = `${0 - description_height}px`;
-    if (_.dataset.selected === "true") {
-      content_el.style.height = `${description_height}px`;
-      description_el.style.marginTop = `${0}px`;
+  styleDrawer();
+};
+
+const styleDrawer = () => {
+  // Set the dynamic styles of the drawer when toggled and when the window is resized
+  const drawer_list = Array.from(document.querySelectorAll(".project-content"));
+  drawer_list.map(drawer_el => {
+    const content_el = drawer_el.querySelector(".project-content-text");
+    const { height } = content_el.getBoundingClientRect();
+    drawer_el.style.height = `${0}px`;
+    content_el.style.marginTop = `${0 - height}px`;
+
+    if (JSON.parse(drawer_el.dataset.selected) === true) {
+      drawer_el.style.height = `${height}px`;
+      content_el.style.marginTop = `${0}px`;
     }
-    return null;
   });
 };
 
-const toggleProjectContent = (e) => {
-  const parent_el = e.currentTarget.parentElement;
-  if (!parent_el.dataset.selected || parent_el.dataset.selected === "false") {
-    Array.from(document.querySelectorAll("[data-selected = 'true']")).map((_) => {
-      _.dataset.selected = "false";
-      return null;
-    });
-    parent_el.dataset.selected = "true";
-  } else {
-    parent_el.dataset.selected = "false";
-  }
-  loadContent();
-};
-
-const loadData = () => {
-  const projects_section = document.querySelector(".projects-section");
-  let project = document.querySelector(".project");
-  Object.entries(projectData).map(([k, v], i) => {
-    if (i !== 0) {
-      project = project.cloneNode(true);
-    }
-    project.querySelector(".project-card-title").textContent = `${v.title}`;
-    project.querySelector(".project-card-img").src = `./assets/icons/icon-${
-      v.icon
-    }`;
-    project.querySelector(".project-content-title").textContent = `${v.title}`;
-    project.querySelector(".project-content-link").href = `${v.link}`;
-    project.querySelector(".project-content-description").textContent = `${
-      v.description
-    }`;
-    project
-      .querySelector(".project-card")
-      .addEventListener("click", toggleProjectContent);
-    project.dataset.selected = "false";
-    return projects_section.appendChild(project);
-  });
-  loadContent();
-};
-
-window.addEventListener("load", loadData);
-window.addEventListener("resize", loadContent);
+window.addEventListener("load", () => {
+  const projects = Array.from(document.querySelectorAll(".project"));
+  projects.map(project => project.addEventListener("click", toggleDrawer));
+  document.body.style.visibility = "visible";
+  styleDrawer();
+});
+window.addEventListener("resize", styleDrawer);
