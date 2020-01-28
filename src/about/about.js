3d88bce7a87c1ff15ff937e2c90160decfdc7089
candicecz <czech.candice@gmail.com>
2020-01-28 15:33:31 -0500

    [UPDATE] about + features styles

--- src/about/about.js
+++ src/about/about.js
@@ -1,3 +1,4 @@
+/* eslint-disable  */
 import "./about.scss";
 import "../partials/partials";
 import teamData from "../team-data.json";
@@ -18,55 +19,67 @@ const loadEmployeeDetails = (id, modal, employee_el) => {
       return links_el.appendChild(link_el);
     });
   }
-  employee_el.querySelector(".employee-name").textContent = `${teamData[id].name}`;
-  employee_el.querySelector(".employee-occupation").textContent = `${teamData[id].occupation}`;
-  employee_el.querySelector(".employee-img").style.backgroundImage =  `url('./assets/team/${teamData[id].imageURL}')`;
+  employee_el.querySelector(
+    ".employee-name"
+  ).textContent = `${teamData[id].name}`;
+  employee_el.querySelector(
+    ".employee-occupation"
+  ).textContent = `${teamData[id].occupation}`;
+  employee_el.querySelector(
+    ".employee-img"
+  ).style.backgroundImage = `url('./assets/team/${teamData[id].imageURL}')`;
 };
 
-
-const toggleModal = (e) => {
+const toggleModal = e => {
   const modal_container = document.querySelector(".modal-container");
   const modal_background = document.querySelector(".modal-background");
   if (e.currentTarget.dataset.modal === "false") {
     const modal_employee = modal_container.querySelector(".modal-employee");
     const modal_close = modal_employee.querySelector(".employee-button");
 
-    document.body.dataset.modal    = "true";
-    modal_employee.dataset.modal   = "true";
+    document.body.dataset.modal = "true";
+    modal_employee.dataset.modal = "true";
     modal_background.dataset.modal = "true";
-    modal_container.dataset.modal  = "true";
+    modal_container.dataset.modal = "true";
 
     modal_close.addEventListener("click", toggleModal);
     modal_background.addEventListener("click", toggleModal);
 
-    loadEmployeeDetails(e.currentTarget.dataset.id, modal_employee.dataset.modal, modal_employee);
+    loadEmployeeDetails(
+      e.currentTarget.dataset.id,
+      modal_employee.dataset.modal,
+      modal_employee
+    );
     return modal_container.appendChild(modal_employee);
   }
 
-  return Array.from(document.querySelectorAll("[data-modal='true']")).map((_) => {
+  return Array.from(document.querySelectorAll("[data-modal='true']")).map(_ => {
     _.dataset.modal = "false";
     return null;
   });
 };
 
+const loadEmployee = () => {
+  return Object.entries(teamData).map(([k, v], i) => {
+    const grid_el = document.querySelector(".team-grid");
+    const affiliate_grid_el = document.querySelector(".affiliate-grid");
+    let employee = document.querySelector(".team-employee");
+    if (i !== 0) {
+      employee = employee.cloneNode(true);
+    }
+    employee.dataset.id = k;
+    employee.dataset.modal = "false";
+    employee.addEventListener("click", toggleModal);
+    if (v.affiliated === true) {
+      affiliate_grid_el.appendChild(employee);
+    } else {
+      grid_el.appendChild(employee);
+    }
+    return loadEmployeeDetails(k, employee.dataset.modal, employee);
+  });
+};
 
-const loadEmployee = Object.entries(teamData).map(([k, v], i) => {
-  const grid_el = document.querySelector(".team-grid");
-  const affiliate_grid_el = document.querySelector(".affiliate-grid");
-  let employee = document.querySelector(".team-employee");
-  if (i !== 0) {
-    employee = employee.cloneNode(true);
-  }
-  employee.dataset.id = k;
-  employee.dataset.modal = "false";
-  employee.addEventListener("click", toggleModal);
-  if (v.affiliated === true) {
-    affiliate_grid_el.appendChild(employee);
-  } else {
-    grid_el.appendChild(employee);
-  }
-  return loadEmployeeDetails(k, employee.dataset.modal, employee);
+window.addEventListener("load", () => {
+  loadEmployee();
+  document.body.style.visibility = "visible";
 });
-
-
-window.addEventListener("load", loadEmployee);
--- src/features/features.js
+++ src/features/features.js
@@ -1,10 +1,12 @@
 import "./features.scss";
-import  handleAnimations  from "../animations";
+import handleAnimations from "../animations";
 import "../partials/partials";
-
 
 const animations = Array.from(document.querySelectorAll(".animation"));
 
-window.addEventListener("load", () => handleAnimations(animations));
+window.addEventListener("load", () => {
+  document.body.style.visibility = "visible";
+  handleAnimations(animations);
+});
 window.addEventListener("scroll", () => handleAnimations(animations));
 window.addEventListener("resize", () => handleAnimations(animations));
